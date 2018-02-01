0f5c42079b4bbbcaf7d289db658f7c312b2fc8c2
jackmega <jackmega@live.com>
2018-02-01 10:07:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function darken() {
+	document.getElementById("html").style.background = "#111"
+}
+function lighten() {
+	document.getElementById("html").style.background = "#eee"
+}
+function large1() {
+	document.getElementById("story1").style.width = "98%";
+	document.getElementById("story2").style.width = "45%";
+	document.getElementById("story3").style.width = "45%";
+}
+
+function large2() {
+	document.getElementById("story2").style.width = "98%";
+	document.getElementById("story3").style.width = "45%";
+	document.getElementById("story1").style.width = "45%";
+}
+
+function large3() {
+	document.getElementById("story3").style.width = "98%";
+	document.getElementById("story1").style.width = "45%";
+	document.getElementById("story2").style.width = "45%";
+}
